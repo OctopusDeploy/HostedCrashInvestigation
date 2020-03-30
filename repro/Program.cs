f75b5e783340cec118b747fc352296f860e23c23
Pawel Pabich <pawel@pabich.eu>
2020-03-30 14:03:29 +1000

    Basics

---
+++ repro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var mutex = new Mutex(true, "ServerTasks-2441_9XSK8QHN7K"))
+            {
+                Console.WriteLine("Inside");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
